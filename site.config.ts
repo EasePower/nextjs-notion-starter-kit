18aac7bcff52da809b7a33df05bd38a75fbd8be9
EasePower <112232572+EasePower@users.noreply.github.com>
2022-09-04 11:46:16 +0800

    test

--- site.config.ts
+++ site.config.ts
@@ -2,16 +2,16 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '7875426197cf461698809def95960ebf',
+  rootNotionPageId: '41a00728ab7046c5a28328f60f5b0cc1',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Next.js Notion Starter Kit',
-  domain: 'nextjs-notion-starter-kit.transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: 'Welcome to my blog',
+  domain: 'Welcome',
+  author: 'Wilbert Yan',
 
   // open graph metadata (optional)
   description: 'Example Next.js Notion Starter Kit Site',
